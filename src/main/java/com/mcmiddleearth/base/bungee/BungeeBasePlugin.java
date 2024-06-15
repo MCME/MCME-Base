cc02826dfe2269ce171bb3812692bd77937e9cf3
EriolEandur <Eriol_Eandur@gmx.de>
2024-06-16 00:38:56 +0200

    fixed bungee plugin main class

--- src/main/java/com/mcmiddleearth/base/bungee/BungeeBasePlugin.java
+++ src/main/java/com/mcmiddleearth/base/bungee/BungeeBasePlugin.java
@@ -1,4 +1,16 @@
 package com.mcmiddleearth.base.bungee;
 
-public class BungeeBasePlugin {
+import net.kyori.adventure.platform.bungeecord.BungeeAudiences;
+import net.kyori.adventure.text.Component;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeBasePlugin extends Plugin {
+
+    BungeeAudiences adventure;
+
+    @Override
+    public void onEnable() {
+        adventure = BungeeAudiences.create(this);
+        adventure.console().sendMessage(Component.text("MCME-Base enabled!!!"));
+    }
 }
