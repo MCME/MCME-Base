4b7f6bc09e850ebdd6bc04429d30ac0ca3a51f10
EriolEandur <Eriol_Eandur@gmx.de>
2024-08-13 22:01:56 +0200

    improved Messages

--- src/main/java/com/mcmiddleearth/base/bungee/command/BungeeMcmeCommandSender.java
+++ src/main/java/com/mcmiddleearth/base/bungee/command/BungeeMcmeCommandSender.java
@@ -1,9 +1,12 @@
 package com.mcmiddleearth.base.bungee.command;
 
 import com.mcmiddleearth.base.adventure.AdventureMessage;
+import com.mcmiddleearth.base.adventure.AdventureUtils;
 import com.mcmiddleearth.base.core.command.McmeCommandSender;
 import com.mcmiddleearth.base.core.message.Message;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.BaseComponent;
@@ -28,7 +31,10 @@ public class BungeeMcmeCommandSender implements McmeCommandSender {
 
     @Override
     public void sendMessage(Message message) {
-        BaseComponent[] baseComponents = BungeeComponentSerializer.get().serialize(((AdventureMessage)message).getComponent());
+        Component component = ((AdventureMessage)message).getComponent();
+        TextColor baseColor = component.color() != null ? component.color() : NamedTextColor.WHITE;
+        BaseComponent[] baseComponents = BungeeComponentSerializer.get()
+                .serialize(AdventureUtils.addBaseColor(baseColor, component));
         //Logger.getGlobal().info("Sender: "+commandSender.toString());
         //Arrays.stream(baseComponents).forEach(component -> Logger.getGlobal().info(component.toLegacyText()));
         commandSender.sendMessage(baseComponents);
